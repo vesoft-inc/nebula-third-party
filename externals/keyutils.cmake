24fea0b8d7312f002da369797ec37e614080cc63
dutor <440396+dutor@users.noreply.github.com>
2019-11-11 20:55:31 +0800

    Added keyutils

---
+++ externals/keyutils.cmake
@@ -0,0 +1,20 @@
+ExternalProject_Add(
+    keyutils
+    URL https://people.redhat.com/dhowells/keyutils/keyutils-1.6.tar.bz2
+    URL_HASH MD5=191987b0ab46bb5b50efd70a6e6ce808
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/keyutils
+    TMP_DIR ${BUILD_INFO_DIR}
+    STAMP_DIR ${BUILD_INFO_DIR}
+    DOWNLOAD_DIR ${DOWNLOAD_DIR}
+    SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/keyutils/source
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    BUILD_IN_SOURCE 1
+    INSTALL_COMMAND make BINDIR=/bin SBINDIR=/sbin
+                         SHAREDIR=/share MANDIR=/man
+                         INCLUDEDIR=/include LIBDIR=/lib
+                         CFLAGS=-fPIC NO_SOLIB=1 DESTDIR=${CMAKE_INSTALL_PREFIX}
+                         install
+    LOG_BUILD 1
+    LOG_INSTALL 1
+)

c5149d8630fa0e1c2e2cb169ae946f7d3f138268
dutor <440396+dutor@users.noreply.github.com>
2019-11-18 13:08:32 +0800

    Fixed linkage with -static-libstdc++ -static-libgcc

--- externals/folly.cmake
+++ externals/folly.cmake
@@ -14,7 +14,7 @@ ExternalProject_Add(
         ${common_cmake_args}
         -DCMAKE_BUILD_TYPE=Release
         -DFOLLY_CXX_FLAGS=-Wno-error
-        -DCMAKE_EXE_LINKER_FLAGS=-latomic
+        "-DCMAKE_EXE_LINKER_FLAGS=-latomic -static-libstdc++ -static-libgcc"
     BUILD_COMMAND make -s -j${BUILDING_JOBS_NUM}
     BUILD_IN_SOURCE 1
     INSTALL_COMMAND make -s -j${BUILDING_JOBS_NUM} install

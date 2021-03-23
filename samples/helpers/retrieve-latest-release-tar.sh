3b8af13caf548e2aadffb7d98178bc1c54b7d511
dutor <440396+dutor@users.noreply.github.com>
2021-03-23 15:35:02 +0800

    update

--- samples/helpers/retrieve-latest-release-tar.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-USER=$1
-REPO=$2
-
-LATEST_URL=https://api.github.com/repos/$USER/$REPO/releases/latest
-
-TAR_URL=$(curl -s $LATEST_URL | grep tarball_url | cut -d\" -f 4)
-
-echo -n $TAR_URL

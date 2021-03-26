442c6c1a63d8c0f1840470aee76743444ae2373e
dutor <440396+dutor@users.noreply.github.com>
2021-03-26 16:51:18 +0800

    patch proxygen to build ARM

--- externals/proxygen.cmake
+++ externals/proxygen.cmake
@@ -15,6 +15,7 @@ ExternalProject_Add(
     TMP_DIR ${BUILD_INFO_DIR}
     STAMP_DIR ${BUILD_INFO_DIR}
     DOWNLOAD_DIR ${DOWNLOAD_DIR}
+    PATCH_COMMAND patch -p1 < ${CMAKE_SOURCE_DIR}/patches/${name}-2021-03-01.patch
     SOURCE_DIR ${source_dir}
     CMAKE_ARGS
             ${common_cmake_args}

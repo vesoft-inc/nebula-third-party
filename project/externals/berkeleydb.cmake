c6f2037cb09a4ccfb59db91ba648711e89ff6211
panda-sheep <59197347+panda-sheep@users.noreply.github.com>
2021-08-19 11:25:00 +0800

    support ldap

---
+++ project/externals/berkeleydb.cmake
@@ -0,0 +1,56 @@
+# Copyright (c) 2021 vesoft inc. All rights reserved.
+#
+# This source code is licensed under Apache 2.0 License,
+# attached with Common Clause Condition 1.0, found in the LICENSES directory.
+
+set(name berkeleydb)
+set(source_dir ${CMAKE_CURRENT_BINARY_DIR}/${name}/source/)
+ExternalProject_Add(
+    ${name}
+    URL http://download.oracle.com/berkeley-db/db-5.1.29.tar.gz
+    URL_HASH MD5=a94ea755ab695bc04f82b94d2e24a1ef
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/${name}/
+    TMP_DIR ${BUILD_INFO_DIR}
+    STAMP_DIR ${BUILD_INFO_DIR}
+    DOWNLOAD_DIR ${DOWNLOAD_DIR}
+    SOURCE_DIR ${source_dir}
+    CONFIGURE_COMMAND
+        ${common_configure_envs}
+        "LIBS=${LIBS}"
+        ./build_unix/../dist/configure ${common_configure_args} --prefix=${CMAKE_BERKELEYDB_INSTALL_PREFIX}
+
+    #message(STATUS "source_dir: ${source_dir}")
+    #message(STATUS "CMAKE_CURRENT_SOURCE_DIR: ${CMAKE_CURRENT_SOURCE_DIR}")
+    #message(STATUS "CMAKE_CURRENT_BINARY_DIR: ${CMAKE_CURRENT_BINARY_DIR}")
+    #message(STATUS "CMAKE_MODULE_PATH: ${CMAKE_MODULE_PATH}")
+    #message(FATAL_ERROR)
+
+    BUILD_IN_SOURCE 1
+    BUILD_COMMAND make -s -j${BUILDING_JOBS_NUM}
+    INSTALL_COMMAND  make -s -j${BUILDING_JOBS_NUM} install
+    LOG_CONFIGURE TRUE
+    LOG_BUILD TRUE
+    LOG_INSTALL TRUE
+)
+
+ExternalProject_Add_Step(${name} setup-compiler
+    DEPENDEES configure
+    DEPENDERS build
+    COMMAND
+        echo "${CMAKE_BERKELEYDB_INSTALL_PREFIX}/lib/"
+            >> /etc/ld.so.conf
+    COMMAND ldconfig -v
+    WORKING_DIRECTORY ${source_dir}
+)
+
+ExternalProject_Add_Step(${name} clean
+    EXCLUDE_FROM_MAIN TRUE
+    ALWAYS TRUE
+    DEPENDEES configure
+    COMMAND make clean -j
+    COMMAND rm -f ${BUILD_INFO_DIR}/${name}-build
+    WORKING_DIRECTORY ${source_dir}
+)
+
+ExternalProject_Add_StepTargets(${name} clean)
+

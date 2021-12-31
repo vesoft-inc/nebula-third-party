014c3b69b3a193fb3e34cd81db6788568baf9767
panda-sheep <59197347+panda-sheep@users.noreply.github.com>
2021-12-31 18:55:32 +0800

    Remove Common Clause License

--- project/check-so-libs.sh
+++ project/check-so-libs.sh
@@ -2,8 +2,7 @@
 
 # Copyright (c) 2021 vesoft inc. All rights reserved.
 #
-# This source code is licensed under Apache 2.0 License,
-# attached with Common Clause Condition 1.0, found in the LICENSES directory.
+# This source code is licensed under Apache 2.0 License.
 
 if (( $# < 1 )); then
   echo "Usage: $0 <install_dir>"

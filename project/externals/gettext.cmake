67b4e82a798bb42a76c8f7caaa180ba2ed9449e2
panda-sheep <59197347+panda-sheep@users.noreply.github.com>
2022-01-04 19:05:54 +0800

    Remove Common Clause License (#30)

--- project/externals/gettext.cmake
+++ project/externals/gettext.cmake
@@ -1,7 +1,6 @@
 # Copyright (c) 2019 vesoft inc. All rights reserved.
 #
-# This source code is licensed under Apache 2.0 License,
-# attached with Common Clause Condition 1.0, found in the LICENSES directory.
+# This source code is licensed under Apache 2.0 License.
 
 set(name gettext)
 set(source_dir ${CMAKE_CURRENT_BINARY_DIR}/${name}/source)

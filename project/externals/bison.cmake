424c3fb0ae852116dc611f1d53336eecc9813dd6
yixinglu <2520865+yixinglu@users.noreply.github.com>
2022-03-03 10:20:34 +0800

    Fix bison datarootdir configuration

--- project/externals/bison.cmake
+++ project/externals/bison.cmake
@@ -6,7 +6,7 @@ set(name bison)
 set(source_dir ${CMAKE_CURRENT_BINARY_DIR}/${name}/source)
 ExternalProject_Add(
     ${name}
-    URL http://ftp.gnu.org/gnu/bison//bison-3.7.6.tar.gz
+    URL http://ftp.gnu.org/gnu/bison/bison-3.7.6.tar.gz
     URL_HASH MD5=da2df6cf3ed0a7b33fdc09e829c74b4b
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}/${name}
     TMP_DIR ${BUILD_INFO_DIR}
@@ -16,6 +16,7 @@ ExternalProject_Add(
     CONFIGURE_COMMAND
         ${common_configure_envs}
         ./configure ${common_configure_args}
+                    --datarootdir=${CMAKE_INSTALL_PREFIX}/share/bison
                     --quiet
     BUILD_COMMAND make -s -j${BUILDING_JOBS_NUM}
     BUILD_IN_SOURCE 1

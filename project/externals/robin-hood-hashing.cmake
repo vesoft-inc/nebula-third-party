57b4c0d76369b2603f1143d2f195d55787db90b7
Sherman <sherman.ye@vesoft.com>
2022-05-16 17:56:36 +0800

    Third-party 5.0 (#45)
    * [WIP] Third-party 5.0
    
    Added libev-4.33 and robin-hood-hashing
    
    * Third-party 5.0
    
    1) Added libev and Robin-hood-hashing
    2) Added support to use archive files for projects cloned directly from the github repository
    
    * Fixed conflicts between libev and libevent
    
    * Fixed libev conflicts
    
    Disabled the event.{c|h} from libev to avoid conflicts with libevent

---
+++ project/externals/robin-hood-hashing.cmake
@@ -0,0 +1,34 @@
+# Copyright (c) 2019 vesoft inc. All rights reserved.
+#
+# This source code is licensed under Apache 2.0 License.
+
+set(name robin-hood-hashing)
+set(source_dir ${CMAKE_CURRENT_BINARY_DIR}/${name}/source)
+ExternalProject_Add(
+    ${name}
+    URL https://github.com/martinus/robin-hood-hashing/archive/refs/tags/3.11.5.tar.gz
+    URL_HASH MD5=a78bd30a7582f25984f8592652836467
+    DOWNLOAD_NAME robin-hood-hashing-3.11.5.tar.gz
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/${name}
+    TMP_DIR ${BUILD_INFO_DIR}
+    STAMP_DIR ${BUILD_INFO_DIR}
+    DOWNLOAD_DIR ${DOWNLOAD_DIR}
+    SOURCE_DIR ${source_dir}
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    BUILD_IN_SOURCE 1
+    INSTALL_COMMAND cp src/include/robin_hood.h ${CMAKE_INSTALL_PREFIX}/include
+    LOG_CONFIGURE TRUE
+    LOG_BUILD TRUE
+    LOG_INSTALL TRUE
+)
+
+ExternalProject_Add_Step(${name} clean
+    EXCLUDE_FROM_MAIN TRUE
+    ALWAYS TRUE
+    DEPENDEES configure
+    COMMAND rm -f ${BUILD_INFO_DIR}/${name}-build
+    WORKING_DIRECTORY ${source_dir}
+)
+
+ExternalProject_Add_StepTargets(${name} clean)

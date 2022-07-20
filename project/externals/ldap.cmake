62d64874e132083a401c7a841258f0086f738c3d
panda-sheep <59197347+panda-sheep@users.noreply.github.com>
2022-07-20 13:36:51 +0800

    upgrade ldap version for suppressing compile warnings

--- project/externals/ldap.cmake
+++ project/externals/ldap.cmake
@@ -6,20 +6,20 @@ set(name ldap)
 set(source_dir ${CMAKE_CURRENT_BINARY_DIR}/${name}/source)
 ExternalProject_Add(
     ${name}
-    URL https://www.openldap.org/software/download/OpenLDAP/openldap-release/openldap-2.4.49.tgz
-    URL_HASH MD5=2a47a6bb4319357ea7b032c45283e79e
+    URL https://www.openldap.org/software/download/OpenLDAP/openldap-release/openldap-2.5.5.tgz
+    URL_HASH MD5=4f32d78070b0fdaba3e2b072b729912a
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}/${name}
     TMP_DIR ${BUILD_INFO_DIR}
     STAMP_DIR ${BUILD_INFO_DIR}
     DOWNLOAD_DIR ${DOWNLOAD_DIR}
     SOURCE_DIR ${source_dir}
     CONFIGURE_COMMAND
-        ${common_configure_envs}
-        ./configure ${common_configure_args}
-                    --enable-syslog
-                    --enable-modules=no
-                    --enable-shared=no
-                    --with-tls
+    ${common_configure_envs}
+    ./configure ${common_configure_args}
+    --enable-syslog
+    --enable-modules=no
+    --enable-shared=no
+    --with-tls
     BUILD_IN_SOURCE 1
     BUILD_COMMAND make -s -j${BUILDING_JOBS_NUM}
     INSTALL_COMMAND make -s -j${BUILDING_JOBS_NUM} install
@@ -28,17 +28,16 @@ ExternalProject_Add(
     LOG_INSTALL TRUE
 )
 
-#update before configure
-#ExternalProject_Add_Step(${name} updateldconfig
-#    DEPENDEES download
-#    DEPENDERS configure
-#    COMMAND
-#        echo "${BERKELEYDB_LIB_DIR}"
-#            >> /etc/ld.so.conf
-#    COMMAND ldconfig -v
-#    WORKING_DIRECTORY ${source_dir}
-#)
-
+# update before configure
+# ExternalProject_Add_Step(${name} updateldconfig
+# DEPENDEES download
+# DEPENDERS configure
+# COMMAND
+# echo "${BERKELEYDB_LIB_DIR}"
+# >> /etc/ld.so.conf
+# COMMAND ldconfig -v
+# WORKING_DIRECTORY ${source_dir}
+# )
 ExternalProject_Add_Step(${name} clean
     EXCLUDE_FROM_MAIN TRUE
     ALWAYS TRUE
@@ -49,4 +48,3 @@ ExternalProject_Add_Step(${name} clean
 )
 
 ExternalProject_Add_StepTargets(${name} clean)
-

ecf340670db08233705d941a2c54b27dd85034dd
dutor <440396+dutor@users.noreply.github.com>
2022-08-27 10:21:10 +0800

    Upgraded RocksDB to 7.5 (#60) (#61)

--- project/externals/googletest.cmake
+++ project/externals/googletest.cmake
@@ -6,9 +6,9 @@ set(name googletest)
 set(source_dir ${CMAKE_CURRENT_BINARY_DIR}/${name}/source)
 ExternalProject_Add(
     ${name}
-    URL https://github.com/google/googletest/archive/refs/tags/release-1.11.0.tar.gz
-    URL_HASH MD5=e8a8df240b6938bb6384155d4c37d937
-    DOWNLOAD_NAME  googletest-1.11.0.tar.gz
+    URL https://github.com/google/googletest/archive/refs/tags/release-1.12.1.tar.gz
+    URL_HASH MD5=e82199374acdfda3f425331028eb4e2a
+    DOWNLOAD_NAME googletest-1.12.1.tar.gz
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}/${name}
     TMP_DIR ${BUILD_INFO_DIR}
     STAMP_DIR ${BUILD_INFO_DIR}

e1e04e9ff8a3cd91bfc65ccf5b2ce0d2b80dd009
Hao Wen <19355821+wenhaocs@users.noreply.github.com>
2023-04-01 09:47:50 +0800

    remove messages

--- project/externals/rocksdb-cloud-update-headers.cmake
+++ project/externals/rocksdb-cloud-update-headers.cmake
@@ -1,25 +1,17 @@
-# Copyright (c) 2019 vesoft inc. All rights reserved.
+# Copyright (c) 2023 vesoft inc. All rights reserved.
 #
 # This source code is licensed under Apache 2.0 License.
 
-# Update the include in headers to reflect the header folder name change
+# Update the #include in headers to reflect the header folder name change
 set(headers_directory "${INSTALL_PREFIX}/include/rocksdb-cloud")
 
 file(GLOB_RECURSE header_files
     LIST_DIRECTORIES false
     "${headers_directory}/*.h"
 )
-list(LENGTH header_files file_num)
-message(STATUS "headers directory: ${headers_directory}")
-#message(STATUS "header files: ${header_files}")
-message(STATUS "header file num: ${file_num}")
 
-foreach(header_file ${header_files})
-	#    message(STATUS "header file: ${header_file}")
-    
+foreach(header_file ${header_files})    
     file(READ ${header_file} contents)
-
     string(REPLACE "#include \"rocksdb" "#include \"rocksdb-cloud" new_contents "${contents}")
-
     file(WRITE ${header_file} "${new_contents}")
 endforeach()
--- project/externals/rocksdb-cloud.cmake
+++ project/externals/rocksdb-cloud.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2019 vesoft inc. All rights reserved.
+# Copyright (c) 2023 vesoft inc. All rights reserved.
 #
 # This source code is licensed under Apache 2.0 License.
 
@@ -69,4 +69,3 @@ ExternalProject_Add_Step(${name} clean
 )
 
 ExternalProject_Add_StepTargets(${name} clean)
-

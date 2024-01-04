9cbb3eef123a6c360e3f6bb92a99bb6097ab7451
mingquan.ji <6930445+nevermore3@users.noreply.github.com>
2024-01-04 20:58:45 +0800

    add date

---
+++ project/externals/date.cmake
@@ -0,0 +1,43 @@
+# Copyright (c) 2024 vesoft inc. All rights reserved.
+#
+# This source code is licensed under Apache 2.0 License.
+
+set(name date)
+set(source_dir ${CMAKE_CURRENT_BINARY_DIR}/${name}/source)
+ExternalProject_Add(
+    ${name}
+    URL https://github.com/HowardHinnant/date/archive/refs/tags/v3.0.1.tar.gz
+    URL_HASH MD5=78902f47f7931a3ae8a320e0dea1f20a
+    DOWNLOAD_NAME date-3.0.1.tar.gz
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/${name}
+    TMP_DIR ${BUILD_INFO_DIR}
+    STAMP_DIR ${BUILD_INFO_DIR}
+    DOWNLOAD_DIR ${DOWNLOAD_DIR}
+    SOURCE_DIR ${source_dir}
+    CONFIGURE_COMMAND
+        ${common_configure_envs}
+        ./configure ${common_configure_args}
+                    --enable-shared --disable-static
+    CMAKE_ARGS
+        ${common_cmake_args}
+        -DBUILD_TZ_LIB=ON
+        -DENABLE_DATE_TESTING=OFF
+        -DCMAKE_BUILD_TYPE=Release
+    BUILD_IN_SOURCE 1
+    BUILD_COMMAND make -s -j${BUILDING_JOBS_NUM}
+    INSTALL_COMMAND make -s -j${BUILDING_JOBS_NUM} install
+    LOG_CONFIGURE TRUE
+    LOG_BUILD TRUE
+    LOG_INSTALL TRUE
+)
+
+ExternalProject_Add_Step(${name} clean
+    EXCLUDE_FROM_MAIN TRUE
+    ALWAYS TRUE
+    DEPENDEES configure
+    COMMAND make clean -j
+    COMMAND rm -f ${BUILD_INFO_DIR}/${name}-build
+    WORKING_DIRECTORY ${source_dir}
+)
+
+ExternalProject_Add_StepTargets(${name} clean)

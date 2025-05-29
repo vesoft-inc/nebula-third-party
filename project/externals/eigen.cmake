02769d30b458790e8aa020d32e5981c5794e9ac1
codesigner <1256658+codesigner@users.noreply.github.com>
2025-05-29 19:51:25 +0800

    add eigen

---
+++ project/externals/eigen.cmake
@@ -0,0 +1,35 @@
+# Copyright (c) 2019 vesoft inc. All rights reserved.
+#
+# This source code is licensed under MIT License.
+
+set(name yaml-cpp)
+set(source_dir ${CMAKE_CURRENT_BINARY_DIR}/${name}/source)
+ExternalProject_Add(
+    ${name}
+    URL https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.zip
+    URL_HASH MD5=a83cb9a2cbba2dd52c137ac62d33d847
+    DOWNLOAD_NAME eigen-3.4.0.zip
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/${name}
+    TMP_DIR ${BUILD_INFO_DIR}
+    STAMP_DIR ${BUILD_INFO_DIR}
+    DOWNLOAD_DIR ${DOWNLOAD_DIR}
+    SOURCE_DIR ${source_dir}
+    BUILD_IN_SOURCE 1
+    CMAKE_ARGS
+        ${common_cmake_args}
+        -DCMAKE_BUILD_TYPE=Release
+    LOG_CONFIGURE TRUE
+    LOG_BUILD TRUE
+    LOG_INSTALL TRUE
+)
+
+ExternalProject_Add_Step(${name} clean
+    EXCLUDE_FROM_MAIN TRUE
+    ALWAYS TRUE
+    DEPENDEES configure
+    COMMAND make clean -j
+    COMMAND rm -f ${BUILD_INFO_DIR}/${name}-build
+    WORKING_DIRECTORY ${source_dir}
+)
+
+ExternalProject_Add_StepTargets(${name} clean)

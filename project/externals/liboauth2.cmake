929ed3f743df9d1ec0521d98b5d0d183a06d9de9
nevermore3 <6930445+nevermore3@users.noreply.github.com>
2025-05-29 15:46:11 +0800

    add liboauth2

--- project/externals/liboauth2.cmake
+++ project/externals/liboauth2.cmake
@@ -1,29 +1,36 @@
-# Copyright (c) 2019 vesoft inc. All rights reserved.
+# Copyright (c) 2025 vesoft inc. All rights reserved.
 #
 # This source code is licensed under Apache 2.0 License.
-
-set(name pkgconf)
+set(name liboauth2)
 set(source_dir ${CMAKE_CURRENT_BINARY_DIR}/${name}/source)
+
 ExternalProject_Add(
     ${name}
-    URL https://github.com/pkgconf/pkgconf/archive/refs/tags/pkgconf-2.4.3.tar.gz
-    URL_HASH MD5=b1e760e20492381607728f58430349ea
-    DOWNLOAD_NAME pkgconf-2.4.3.tar.gz
-    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/pkgconf
+    URL https://github.com/OpenIDC/liboauth2/releases/download/v2.1.0/liboauth2-2.1.0.tar.gz
+    URL_HASH MD5=8b74e19a45594d0f0e9c60a457fb867b
+    DOWNLOAD_NAME liboauth2-2.1.0.tar.gz
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/${name}
     TMP_DIR ${BUILD_INFO_DIR}
     STAMP_DIR ${BUILD_INFO_DIR}
     DOWNLOAD_DIR ${DOWNLOAD_DIR}
-    SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/pkgconf/source
+    SOURCE_DIR ${source_dir}
     CONFIGURE_COMMAND
         ${common_configure_envs}
-        ./autogen.sh && ./configure ${common_configure_args}
-    BUILD_IN_SOURCE 1
+        ./configure ${common_configure_args}
+                    --without-nginx
+                    --without-apache
+                    --without-memcache
+                    --with-redis
+                    --enable-shared
+                    --disable-static
     BUILD_COMMAND make -s -j${BUILDING_JOBS_NUM}
+    BUILD_IN_SOURCE 1
     INSTALL_COMMAND make -s -j${BUILDING_JOBS_NUM} install
     LOG_CONFIGURE TRUE
     LOG_BUILD TRUE
     LOG_INSTALL TRUE
 )
+
 
 ExternalProject_Add_Step(${name} clean
     EXCLUDE_FROM_MAIN TRUE

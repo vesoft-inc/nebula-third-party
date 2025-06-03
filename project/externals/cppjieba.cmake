1c098fd947d4815a61cacf632e3bcf98068aaf68
Shinji-IkariG <george.qiao@vesoft.com>
2025-06-03 16:34:54 +0800

    fix-jieba

--- project/externals/cppjieba.cmake
+++ project/externals/cppjieba.cmake
@@ -14,11 +14,11 @@ ExternalProject_Add(
     STAMP_DIR ${BUILD_INFO_DIR}
     DOWNLOAD_DIR ${DOWNLOAD_DIR}
     SOURCE_DIR ${source_dir}
-    BUILD_IN_SOURCE 0
-    CMAKE_ARGS
-        ${common_cmake_args}
-        -DBUILD_TESTS=OFF
-        -DWITH_PYTHON=OFF
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/include
+            COMMAND ${CMAKE_COMMAND} -E copy_directory ${source_dir}/include ${CMAKE_INSTALL_PREFIX}/include
+    BUILD_IN_SOURCE 1
     LOG_CONFIGURE TRUE
     LOG_BUILD TRUE
     LOG_INSTALL TRUE
@@ -28,7 +28,7 @@ ExternalProject_Add_Step(${name} clean
     EXCLUDE_FROM_MAIN TRUE
     ALWAYS TRUE
     DEPENDEES configure
-    COMMAND make clean -j
+    COMMAND make clean
     COMMAND rm -f ${BUILD_INFO_DIR}/${name}-build
     WORKING_DIRECTORY ${source_dir}
 )

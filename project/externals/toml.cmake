d2be87600abd1ebe4648905f67db1112ea7d4276
nevermore3 <6930445+nevermore3@users.noreply.github.com>
2025-06-25 16:11:47 +0800

    tomlplusplus

---
+++ project/externals/toml.cmake
@@ -0,0 +1,36 @@
+# Copyright (c) 2025 vesoft inc. All rights reserved.
+#
+# This source code is licensed under Apache 2.0 License.
+
+set(name toml)
+set(source_dir ${CMAKE_CURRENT_BINARY_DIR}/${name}/source)
+ExternalProject_Add(
+    ${name}
+    URL https://github.com/marzer/tomlplusplus/archive/refs/tags/v3.4.0.tar.gz
+    URL_HASH MD5=c1f32ced14311fe949b9ce7cc3f7a867
+    DOWNLOAD_NAME tomlplusplus-3.4.0.tar.gz
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/${name}
+    TMP_DIR ${BUILD_INFO_DIR}
+    STAMP_DIR ${BUILD_INFO_DIR}
+    DOWNLOAD_DIR ${DOWNLOAD_DIR}
+    SOURCE_DIR ${source_dir}
+    CMAKE_ARGS
+        ${common_cmake_args}
+    BUILD_IN_SOURCE 1
+    BUILD_COMMAND make -s -j${BUILDING_JOBS_NUM}
+    INSTALL_COMMAND make -s -j${BUILDING_JOBS_NUM} install
+    LOG_CONFIGURE TRUE
+    LOG_BUILD TRUE
+    LOG_INSTALL TRUE
+)
+
+ExternalProject_Add_Step(${name} clean
+    EXCLUDE_FROM_MAIN TRUE
+    ALWAYS TRUE
+    DEPENDEES configure
+    COMMAND make clean -j
+    COMMAND rm -f ${BUILD_INFO_DIR}/${name}-build
+    WORKING_DIRECTORY ${source_dir}
+)
+
+ExternalProject_Add_StepTargets(${name} clean)
